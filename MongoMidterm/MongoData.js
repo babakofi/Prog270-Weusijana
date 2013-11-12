bc7271368bc699143c862cecd5032b63206eb6e1
Baba Kofi Weusijana <kofi@probook.(none)>
2013-11-12 03:57:51 -0800

    Added MongoMidterm

---
+++ MongoMidterm/MongoData.js
@@ -0,0 +1,52 @@
+/**
+ * @author Charlie Calvert
+ */
+
+
+angular.module('elvenApp', ['pres'])
+.controller('MyController', function($scope, $http, presidents) {
+    $scope.hint = "<p>Start with <strong>node server.js</strong> to retrieve JSON from Server</p>";
+    
+    // $scope.presidents = presidents;
+    $scope.presidents = presidents.query({}, function(users) {
+      $scope.presidentsLength = $scope.presidents.length;
+      console.log($scope.presidentsLength);
+    });
+	
+	var getDataJson = $http.get('data.json');
+
+	getDataJson.success(function(data, status, headers, config)  {
+		$scope.data = data;
+	});
+	
+	getDataJson.error(function(data, status, headers, config) {
+		throw new Error('Oh no! An Error!');
+	});
+
+});
+
+angular.module('pres', ['ngResource'])
+.factory('presidents', function($resource) {
+	console.log('Presidents factory called');
+	var Presidents = $resource('https://api.mongolab.com/api/1/databases/elvenlab01/collections/Presidents/:id', {
+      // apiKey:'4fb51e55e4b02e56a67b0b66',
+      apiKey:'qfSxFoUGHBA1EuUlqhux_op2fy6oF_wy',
+      id:'@_id.$oid'
+    });
+
+    Presidents.prototype.getPresidentName = function() {
+      return this.presidentName;
+    };
+    
+    Presidents.prototype.getTermStart = function() {
+    	return this.termStart;
+    };
+    
+    Presidents.prototype.getTermEnd = function() {
+    	return this.termEnd;
+    };
+
+    return Presidents;    
+	 
+	// return { a: 2 };		
+});
